7bffc52c2200dd874c21a9eb7d200c8e5ce5eebe
Maro <maro@pop-os.localdomain>
2022-05-14 11:30:14 +0300

    Initial commit

---
+++ src/main/java/fi/maro/wiktionaryproxy/WiktionaryProxyApplication.java
@@ -0,0 +1,13 @@
+package fi.maro.wiktionaryproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiktionaryProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiktionaryProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/maro/wiktionaryproxy/WiktionaryProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.maro.wiktionaryproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiktionaryProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
